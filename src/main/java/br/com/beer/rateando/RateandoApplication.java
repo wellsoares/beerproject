39a926cb3b419380d45ca3987fa17617786061bc
wellington soares nunes <well_002@yahoo.com.br>
2018-12-16 02:12:12 -0200

    First Commit

---
+++ src/main/java/br/com/beer/rateando/RateandoApplication.java
@@ -0,0 +1,14 @@
+package br.com.beer.rateando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateandoApplication.class, args);//
+	}
+
+}
+
